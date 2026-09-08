766ffa4999d038eba0cbefe4887dbc0808278ad7
XCodeSonic <xcodesonic@gmail.com>
2026-09-08 13:17:02 +0800

    fix cloudflare link axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://154.7.228.161/septomic-api',
+  baseURL: 'https://xcodeepoints.xyz/septomic-api',
 })
 
 api.interceptors.request.use((config) => {
